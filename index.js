6146d6baf84dd0fb15484baebc62595e70c20375
Alex <aleksanderst@abv.bg>
2023-08-01 20:00:09 +0300

    test push

--- index.js
+++ index.js
@@ -1,11 +1,2 @@
-// When the user clicks the purchase button, render out
-// "Something went wrong, please try again" in the paragraph
-// that has the id="error".
-
-let errorParagraph = document.getElementById("error")
-console.log(errorParagraph)
-
-function purchase() {
-    console.log("button clicked")
-    errorParagraph.
-}
+let GuestScoreEl = document.getElementById("Guest-Score")
+console.log(GuestScoreEl)
