e45bac0b31221e4bda4ac576185c3311872dc6b1
Alex <aleksanderst@abv.bg>
2023-08-01 20:25:32 +0300

    HomeAddOne button test2

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ let HomeAddOneEl = document.getElementById("Home-AddOne")
 let GuestScoreEl = document.getElementById("Guest-Score")
 
 HomeAddOne = 1
+HomeScoreEl.textContent = 0
 function HomeAddOne(){ 
-    HomeScoreEl.textContent = 0
     HomeScoreEl.textContent += HomeAddOne
 }
