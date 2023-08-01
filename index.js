5f8c1f85d1defe63eabbc2639cb8af66a58c0cdf
Alex <aleksanderst@abv.bg>
2023-08-01 20:21:11 +0300

    Implemented HomeAddOne button

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
+let HomeScoreEl = document.getElementById("Home-Score")
+let HomeAddOneEl = document.getElementById("Home-AddOne")
+
 let GuestScoreEl = document.getElementById("Guest-Score")
-console.log(GuestScoreEl)
+
+
+function HomeAddOne(){
+    HomeAddOne = 1
+    HomeScoreEl.textContent = 0
+    HomeScoreEl.textContent += HomeAddOne
+}
